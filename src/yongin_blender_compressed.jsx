c477492675447cf1027b0470c36c02545745dec5
rainbow <rainbowtech.rnd@gmail.com>
2023-08-14 17:01:31 +0900

    added jsx for 350mb model

--- src/yongin_blender_compressed.jsx
+++ src/yongin_blender_compressed.jsx
@@ -1,29 +1,7 @@
-import "./styles.css";
-import { Canvas } from "@react-three/fiber";
-import {  OrbitControls } from "@react-three/drei";
-import { Suspense } from "react";
-// import { Model } from "./yongin_blender_compressed";
-
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
-// export function Model(props) {
-//     const { nodes, materials } = useGLTF("/yongin_compressed.glb");
-//     return (
-//         <group {...props} dispose={null}>
-//             <mesh
-//                 castShadow
-//                 receiveShadow
-//                 geometry={nodes.Tile_1.geometry}
-//                 material={nodes.Tile_1.material}
-//             />
-
-//         </group>
-//     );
-// }
-
-// useGLTF.preload("/yongin_compressed.glb");
-export  function Model(props) {
+export default function Model(props) {
   const { nodes, materials } = useGLTF("/yongin_compressed_400.gltf");
   return (
     <group {...props} dispose={null}>
@@ -218,20 +196,3 @@ export  function Model(props) {
 }
 
 useGLTF.preload("/yongin_compressed_400.gltf");
-
-
-
-export default function App() {
-  return (
-    <div className="App">
-      <Canvas>
-          <ambientLight />
-        <Suspense fallback={null}>
-          <Model scale={0.07} />
-          <OrbitControls />
-        </Suspense>
-      </Canvas>
-    </div>
-  );
-}
-0
